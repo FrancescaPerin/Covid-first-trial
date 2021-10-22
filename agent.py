9471dd4697866f0adc7de5ffbfb9ff70d42f023e
FrancescaPerin <perinfrancesca9@gmail.com>
2021-10-22 17:05:12 +0200

    refactoring pt2

--- agent.py
+++ agent.py
@@ -99,23 +99,6 @@ class Nation(Agent):
 
 		return self,conn_agents
 
-class NationCities(Agent):
-
-	def interact(self, conn_agents, value):
-
-		migration= int(self.state.N * 0.4)
-
-		self.emigrate(migration)
-
-		for agent in conn_agents:
-
-			new_state_agent = agent.immigrate(self, migration/len(conn_agents))
-
-
-			agent.replace_state(new_state_agent)
-
-
-		return self,conn_agents
 
 
 
