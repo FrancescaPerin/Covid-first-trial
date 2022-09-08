ebc1a8e1ee16325aa01c256d02753a6c48c2ce6a
daddabarba <davidebarbieri97@gmail.com>
2022-09-08 21:44:06 +0200

    Added Nation plotting code to bash_plot.sh

--- src/code/bash_plot.sh
+++ src/code/bash_plot.sh
@@ -1,11 +1,43 @@
+#! /bin/bash
+
 #Nation
 
-python make_plots.py --result_dirs ../../../results_max/2022_Aug_25_19_23_14 ../../../results_max/2022_Aug_25_19_23_20 ../../../results_max/2022_Aug_25_19_23_25 ../../../results_max/2022_Aug_25_19_23_31 ../../../results_max/2022_Aug_25_19_23_37 --output_dirs plots/Nation/set_1 --aggregate --file-value settings.json lockdown_period
+# Plots for settings_Nation_set1.json
+results_nation_1=( 2022_Sep_08_20_48_08 2022_Sep_08_20_48_50 2022_Sep_08_20_49_31 2022_Sep_08_20_50_12 2022_Sep_08_20_50_54 )
 
-python make_plots.py --result_dirs ../../../results_max/2022_Aug_25_19_26_35/ ../../../results_max/2022_Aug_25_19_26_55 ../../../results_max/2022_Aug_25_19_27_01 ../../../results_max/2022_Aug_25_19_27_08 ../../../results_max/2022_Aug_25_19_27_16 --output_dirs plots/Nation/set_2 --total_group --aggregate --file-value settings.json lockdown_period
+python make_plots.py \
+       --result_dirs $(for dir in "${results_nation_1[@]}"; do echo ../../results/"${dir}"; done) \
+       --output_dirs plots/Nation/set_1 \
+       --aggregate --file-value settings.json lockdown_period
 
-python make_plots.py --result_dirs ../../../results_max/2022_Aug_25_19_35_24 ../../../results_max/2022_Aug_25_19_35_31 ../../../results_max/2022_Aug_25_19_35_38 ../../../results_max/2022_Aug_25_19_35_44 ../../../results_max/2022_Aug_25_19_35_51 --output_dirs plots/Nation/set_3 --aggregate --file-value settings.json lockdown_period
+echo "Made plots for Nation set 1"
 
-python make_plots.py --result_dirs ../../../results_max/2022_Aug_25_19_40_08 ../../../results_max/2022_Aug_25_19_40_15 ../../../results_max/2022_Aug_25_19_40_22 ../../../results_max/2022_Aug_25_19_40_29 ../../../results_max/2022_Aug_25_19_40_36 --output_dirs plots/Nation/set_4 --total_group --aggregate --file-value settings.json lockdown_period 
+# Plots for settings_Nation_set2.json
+results_nation_2=( 2022_Sep_08_20_51_38 2022_Sep_08_20_52_21 2022_Sep_08_20_52_21 2022_Sep_08_20_53_46 2022_Sep_08_20_54_30 )
 
-#NationRL
+python make_plots.py \
+     --result_dirs $(for dir in "${results_nation_2[@]}"; do echo ../../results/"${dir}"; done) \
+     --output_dirs plots/Nation/set_2 \
+     --aggregate --file-value settings.json lockdown_period --total_group
+
+echo "Made plots for Nation set 2"
+
+# Plots for settings_Nation_set3.json
+results_nation_3=( 2022_Sep_08_20_55_30  2022_Sep_08_20_56_30 2022_Sep_08_20_57_30 2022_Sep_08_20_58_33 2022_Sep_08_20_59_34 )
+
+python make_plots.py \
+     --result_dirs $(for dir in "${results_nation_3[@]}"; do echo ../../results/"${dir}"; done) \
+     --output_dirs plots/Nation/set_3 \
+     --aggregate --file-value settings.json lockdown_period
+
+echo "Made plots for Nation set 3"
+
+# Plots for settings_Nation_set4.json
+results_nation_4=( 2022_Sep_08_21_00_35 2022_Sep_08_21_01_37 2022_Sep_08_21_02_39 2022_Sep_08_21_03_43 2022_Sep_08_21_04_45 )
+
+python make_plots.py \
+     --result_dirs $(for dir in "${results_nation_4[@]}"; do echo ../../results/"${dir}"; done) \
+     --output_dirs plots/Nation/set_4 \
+     --aggregate --file-value settings.json lockdown_period --total_group
+
+echo "Made plots for Nation set 4"
